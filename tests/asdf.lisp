2f9b14463ecb8e624acd9afe323e36bc20e56de6
Mark Cox <markcox80@gmail.com>
2020-03-28 14:12:29 +1000

    Use 5am:run! instead of 5am:run to get a report.

--- tests/asdf.lisp
+++ tests/asdf.lisp
@@ -1,5 +1,5 @@
 (in-package "DATA-FLOW.TESTS")
 
 (defmethod asdf:perform ((op asdf:test-op) (c (eql (asdf:find-system "data-flow/tests"))))
-  (5am:run 'data-flow.tests:all-data-flow-tests)
+  (5am:run! 'data-flow.tests:all-data-flow-tests)
   (values))
