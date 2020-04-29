b3f87a81f3c59bb3462e6d897a5c8c17b3645fba
Mark Cox <markcox80@gmail.com>
2020-04-30 06:43:59 +1000

    Export symbols from DATA-FLOW.RESOURCE-SCHEDULER.

--- schedulers/packages.lisp
+++ schedulers/packages.lisp
@@ -1,2 +1,9 @@
 (defpackage "DATA-FLOW.RESOURCE-SCHEDULER"
-  (:use "COMMON-LISP"))
+  (:use "COMMON-LISP")
+
+  (:export "*DEFAULT-POLL-SECONDS*"
+           "RESOURCES-SCHEDULER"
+           "MAKE-RESOURCES-SCHEDULER"
+           "RESOURCES"
+           "NUMBER-OF-THREADS"
+           "THREADS"))
