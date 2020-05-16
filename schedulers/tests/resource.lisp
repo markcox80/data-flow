10d206ba9cf404586d016ee665b166ec059a50cd
Mark Cox <markcox80@gmail.com>
2020-05-17 09:41:26 +1000

    Added comment about the use of barriers so I do not forget.

--- schedulers/tests/resource.lisp
+++ schedulers/tests/resource.lisp
@@ -18,6 +18,20 @@
     (finishes (data-flow:schedule scheduler (constantly 3) :required-resources 0))))
 
 (test blocking-allowed-p
+  ;; The original intent of this test is to have concurrent tasks
+  ;; check if blocking was allowed. Unfortunately, this is a bit
+  ;; tricky without the use of barriers. Each task should 1) wait
+  ;; until all threads are executing 2) test if blocking is allowed 3)
+  ;; wait until all threads have determined if blocking is allowed 4)
+  ;; carry on executing.
+  ;;
+  ;; Should the data-flow scheduler API provide support for barriers?
+  ;; Barriers imply the existence of parallelism which is an
+  ;; assumption I do not want in the data-flow system. Furthermore,
+  ;; barriers assume a collection of threads are executing the same
+  ;; code on different data. The data flow system is not meant for
+  ;; this type of paralleism.
+
   (let* ((scheduler (data-flow.resource-scheduler:make-resource-scheduler 5))
          (results (make-array 8 :initial-element '#:unset)))
     (dotimes (i (length results))
@@ -25,6 +39,9 @@
                                       (lambda (scheduler)
                                         (setf (aref results index) (data-flow:blocking-allowed-p scheduler))))))
     (data-flow:execute scheduler)
+
+    ;; This test is rather weak without barriers as it depends on a
+    ;; number of external factors.
     (is-true (plusp (count-if (lambda (value)
                                 (is-true (member value '(nil t)))
                                 (eql value t))
