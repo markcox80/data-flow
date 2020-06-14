29e85e2e284b9be3bb3556308ad4717d197fde02
Mark Cox <markcox80@gmail.com>
2020-06-14 12:35:21 +1000

    Added single reader multiple writer variant of BT-MUTEX-QUEUE.

--- containers/bt-mutex-queue.lisp
+++ containers/bt-mutex-queue.lisp
@@ -6,9 +6,16 @@
 
 (defpackage "DATA-FLOW.BT-MUTEX-QUEUE"
   (:use "COMMON-LISP")
+  ;; Basic Bordeaux Threads Mutex Queue
   (:export "BT-MUTEX-QUEUE"
-           "MAKE-BT-MUTEX-QUEUE"))
+           "MAKE-BT-MUTEX-QUEUE")
+
+  ;; Single Reader Multiple Writer Bordeaux Threads Mutex Queue
+  (:export "SRMW-BT-MUTEX-QUEUE"
+           "MAKE-SRMW-BT-MUTEX-QUEUE"))
 (in-package "DATA-FLOW.BT-MUTEX-QUEUE")
+
+;;;; General Implementation
 
 (defclass bt-mutex-queue (data-flow.queue:queue)
   ((%lock
@@ -39,3 +46,19 @@
 (defmethod data-flow.queue:clear ((queue bt-mutex-queue))
   (bordeaux-threads:with-lock-held ((lock queue))
     (data-flow.queue:clear (inner-queue queue))))
+
+;;;; Single Reader Multiple Writer Implementation
+
+(defclass srmw-bt-mutex-queue (bt-mutex-queue)
+  ((%writer-lock :initarg :writer-lock
+                 :initform (bordeaux-threads:make-lock "DATA-FLOW.BT-MUTEX-QUEUE::SRMW-BT-MUTEX-QUEUE/WRITER")
+                 :reader writer-lock)))
+
+(defun make-srmw-bt-mutex-queue (inner-queue)
+  (check-type inner-queue data-flow.queue:queue)
+  (make-instance 'srmw-bt-mutex-queue
+                 :inner-queue inner-queue))
+
+(defmethod data-flow.queue:enqueue ((queue srmw-bt-mutex-queue) item)
+  (bordeaux-threads:with-lock-held ((writer-lock queue))
+    (call-next-method)))
