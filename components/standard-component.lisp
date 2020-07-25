8c03a5223bd87de36c464ebf9473d21845e01e87
Mark Cox <markcox80@gmail.com>
2020-07-26 07:52:01 +1000

    STANDARD-COMPONENT now inherits STANDARD-PORT-COMPONENT-MIXIN.

--- components/standard-component.lisp
+++ components/standard-component.lisp
@@ -1,4 +1,5 @@
 (in-package "DATA-FLOW.COMPONENT")
 
-(defclass standard-component (basic-component)
+(defclass standard-component (basic-component
+                              data-flow.component.standard-port:standard-port-component-mixin)
   ())
