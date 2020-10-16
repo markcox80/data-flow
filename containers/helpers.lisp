e5a26aa3e134f162092fddd424f0ee0a843b0bbf
Mark Cox <markcox80@gmail.com>
2020-10-17 08:05:52 +1000

    Fixed bug in MAKE-SRMW-FIFO when no threads are available.

--- containers/helpers.lisp
+++ containers/helpers.lisp
@@ -4,6 +4,6 @@
   #+(and data-flow.features:threads data-flow.features:compare-and-set)
   (data-flow.lock-free-fifo:make-lock-free-fifo)
   #+(and data-flow.features:threads (not data-flow.features:compare-and-set))
-  (data-flow.bt-mutex-queue:make-srmw-bt-mutex-queue)
+  (data-flow.bt-mutex-queue:make-srmw-bt-mutex-queue (data-flow.fifo:make-fifo))
   #-data-flow.features:threads
   (data-flow.fifo:make-fifo))
